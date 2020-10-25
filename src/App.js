da4ce610cb9011a727aa2bbe0943c1601ccca990
Marzia <marziadeodato@gmail.com>
2020-10-25 10:15:45 +0000

    installed react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
 import Header from './components/layout/Header';
@@ -55,15 +56,22 @@ class App extends Component {
 
   render () {
     return (
+      <Router>
       <div className="App">
         <div className = "container">
         <Header />
-        <AddTodo addTodo = {this.addTodo} />
-        <Todos todos = {this.state.todos} markComplete = {this.markComplete} 
-        delTodo = {this.delTodo}/>
+        <Route path = "/" render = { props => (
+          <React.Fragment>
+            <AddTodo addTodo = {this.addTodo} />
+            <Todos todos = {this.state.todos} markComplete = {this.markComplete} 
+             delTodo = {this.delTodo}/>
+          </React.Fragment>
+        )} />
+        
 
         </div>
       </div>
+      </Router>
     );
   }
 }
